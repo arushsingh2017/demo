0dc085a06c58edb2979c09439df7005f7272e464
manish <mr.manish212@gmail.com>
2019-06-25 18:49:36 -0700

    Create header.php
    add

---
+++ mahepal-travel/include/header.php
@@ -0,0 +1,90 @@
+        <!--============= TOP-BAR ===========-->
+        <div id="top-bar" class="tb-text-white">
+            <div class="container">
+                <div class="row">          
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                        <div id="info">
+                             <ul class="list-unstyled list-inline">                                
+                                <li><span><i class="fa fa-phone"></i></span>+91: 9479473941,  8004220777</li>
+                                <li><span><i class="fa fa-envelope"></i></span>support@ahoaa.com</li>
+                            </ul>
+                        </div><!-- end info -->
+                    </div><!-- end columns -->
+                    
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                        <div id="links">
+                            <ul class="list-unstyled list-inline">
+                                <li><a href="#"><span><i class="fa fa-facebook"></i></span></a></li>
+                                <li><a href="#"><span><i class="fa fa-twitter"></i></span></a></li>
+                                <li><a href="#"><span><i class="fa fa-google-plus"></i></span></a></li>
+                                <li><a href="#"><span><i class="fa fa-youtube-play"></i></span></a></li>
+                            </ul>
+                        </div><!-- end links -->
+                    </div><!-- end columns -->				
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </div><!-- end top-bar -->
+
+        <nav class="navbar navbar-default main-navbar navbar-custom navbar-colored" id="mynavbar">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" id="menu-button">
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>                        
+                    </button>
+                    <div class="header-search hidden-lg">
+                    	<a href="javascript:void(0)" class="search-button"><span><i class="fa fa-search"></i></span></a>
+                    </div>
+                    <a href="#" class="navbar-brand"><img src="images/mahepal-logo.png" alt="Logo"></a>
+                </div><!-- end navbar-header -->
+                
+                <div class="collapse navbar-collapse" id="myNavbar1">
+                    <ul class="nav navbar-nav navbar-right ">
+                        <li class="dropdown active"><a href="index.php">Home</a></li>
+                        <li class="dropdown"><a href="about.php">About</a></li>
+                        <li class="dropdown"><a href="tours.php">Tours</a></li>
+                        <li class="dropdown"><a href="car-rental.php">Car Rentals</a></li>
+                        <li class="dropdown"><a href="gallery.php">Gallery</a></li>
+                        <li class="dropdown"><a href="#">Get Best Quote</a></li>
+                        <li class="dropdown"><a href="contact.php">Contact Us</a></li>
+                        
+                    </ul>
+                </div><!-- end navbar collapse -->
+            </div><!-- end container -->
+        </nav><!-- end navbar -->
+
+		<div class="sidenav-content">
+            <div id="mySidenav" class="sidenav" >
+                <h2 id="web-name"><span><i class="fa fa-plane"></i></span>Mahepal Travels</h2>
+
+                <div id="main-menu">
+                	<div class="closebtn">
+                        <button class="btn btn-default" id="closebtn">&times;</button>
+                    </div><!-- end close-btn -->
+                    
+                    <div class="list-group panel">
+                    
+                        <a href="index.php" class="list-group-item active" >Home</a>
+                       
+                        
+                        <a href="about.php" class="list-group-item">About</a>
+                       
+                        
+                        <a href="tours.php" class="list-group-item">Tours </a>
+                        
+                        <a href="car-rental.php" class="list-group-item">CAR RENTALS</a>
+                       
+                        
+                        <a href="gallery.php" class="list-group-item"> GALLERY </a>
+                        
+                        <a href="#" class="list-group-item" >GET QUOTE </a>
+                        
+                        
+                        <a href="contact.php" class="list-group-item" >CONTACT US </a>
+                       
+                    </div><!-- end list-group -->
+                </div><!-- end main-menu -->
+            </div><!-- end mySidenav -->
+        </div><!-- end sidenav-content -->
+        
