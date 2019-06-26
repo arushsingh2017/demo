9c2450cc29b5a42415b66d76591e32340b1f29b7
manish <mr.manish212@gmail.com>
2019-06-25 18:49:53 -0700

    Create footer.php
    add

---
+++ mahepal-travel/include/footer.php
@@ -0,0 +1,82 @@
+<!--======================= FOOTER =======================-->
+        <section id="footer" class="ftr-heading-w ftr-heading-mgn-2">
+        
+            <div id="footer-top" class="banner-padding ftr-top-grey ftr-text-grey">
+                    <div class="container clear-padding sm-footer">
+                <div class="col-md-3 col-sm-6 footer-about-box">
+                    <h4>Mahepal Travels</h4>
+                    <p>Lorem ipsum dolor sit amet, ad duo fugit aeque fabulas, in lucilius prodesset pri. Veniam delectus ei vis. Est atqui timeam mnesarchum at, pro an eros perpetua ullamcorper Lorem ipsum dolor sit amet, ad duo fugit aeque fabulas, in lucilius prodesset pri. </p>
+                    <a href="#">READ MORE</a>
+                </div>
+                <div class="col-md-3 col-sm-6 footer-gallery">
+                    <h4>QUICK LINK</h4>
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="about.php">About Us</a></li>
+                            <li><a href="tours.php">Tours</a></li>
+                            <li><a href="car-rental.php">Car Rentals</a></li>
+                            <li><a href="gallery.php">Gallery</a></li>
+                            <li><a href="#">Get Best Quote</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
+
+                        </ul>
+                </div>
+                 <div class="col-md-3 col-sm-6 contact-box">
+                    <h4>CONTACT US</h4>
+                    <p><i class="fa fa-home"></i> B- 55, 2<sup>nd</sup>&nbsp; floor, Lowther Road George town Prayagraj (Allahabad),<br> 211002</p>
+                    <p><i class="fa fa-envelope-o"></i> support@ahoaa.com </p>
+                    <p><i class="fa fa-phone"></i> +91 8004220777 / 9479473941 </p>
+                    
+                </div>
+
+                <div class="clearfix visible-sm-block"></div>
+
+                <div class="col-md-3 col-sm-6 footer-subscribe">
+                    <h4>Follow With Us</h4>
+                    <p class="social-media">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-google-plus"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                    </p>
+                </div>
+            </div>
+            </div>
+            <!-- end footer-top -->
+
+            <div id="footer-bottom" class="ftr-bot-black">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" id="copyright">
+                           <p>&copy; 2019 <a href="#"> Mahepal Travels </a> || All rights reserved.</p>
+                        </div><!-- end columns -->
+                        
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6" id="terms">
+                            <ul class="list-unstyled list-inline">
+                            	<li> <a href="#">Web Curved Technology</a></li>
+                                
+                            </ul>
+                        </div><!-- end columns -->
+                    </div><!-- end row -->
+                </div><!-- end container -->
+            </div><!-- end footer-bottom -->
+            
+        </section><!-- end footer -->
+        
+
+        
+        <!-- Page Scripts Starts -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery.colorpanel.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.flexslider.js"></script>
+        <script src="js/bootstrap-datepicker.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/custom-navigation.js"></script>
+        <script src="js/custom-flex.js"></script>
+        <script src="js/custom-owl.js"></script>
+        <script src="js/custom-date-picker.js"></script>
+        <!-- Page Scripts Ends -->
+        
+    </body>
+</html>
